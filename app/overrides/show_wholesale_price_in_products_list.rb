bed4e92e3794e9d31725f77d134797c07ce7e58b
M. Scott Ford <scott@corgibytes.com>
2012-01-09 09:19:17 -0500

    Removes duplicated product name in wholesale price.

--- app/overrides/show_wholesale_price_in_products_list.rb
+++ app/overrides/show_wholesale_price_in_products_list.rb
@@ -2,5 +2,5 @@ Deface::Override.new(
   :name => "show_wholesale_price_in_products_list",
   :virtual_path => "shared/_products",
   :replace => "#products[data-hook] span.price.selling",
-  :text => %q{<%= link_to raw(product.name + " <span class='price selling'>#{product_price(product)}</span>"), product, :class => 'info' %>}
+  :text => %q{<%= link_to raw("<span class='price selling'>#{product_price(product)}</span>"), product, :class => 'info' %>}
 )
