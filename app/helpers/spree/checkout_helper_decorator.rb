9b8a51579e18599202031906aaa193ef8445ff37
Jeff Squires <jeff.squires@gmail.com>
2012-07-07 17:47:32 -0400

    [Fixes #23] - prevents crash on confirm step

---
+++ app/helpers/spree/checkout_helper_decorator.rb
@@ -0,0 +1,12 @@
+module Spree
+  CheckoutHelper.module_eval do
+    def checkout_states
+      if (@order.is_wholesale? && @order.wholesaler.terms != "Credit Card") ||
+         (@order.payment && @order.payment.payment_method.payment_profiles_supported?)
+        %w(address delivery payment confirm complete)
+      else
+        %w(address delivery payment complete)
+      end
+    end
+  end
+end
