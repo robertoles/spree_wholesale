eb564fa8de3a04047fd72b2239a60c6cc6538cc2
Jeff Dutil <jdutil21@gmail.com>
2012-07-12 12:48:38 -0400

    Redirect to the admins wholesaler show action not the front end.

--- app/controllers/spree/admin/wholesalers_controller.rb
+++ app/controllers/spree/admin/wholesalers_controller.rb
@@ -42,7 +42,9 @@ class Spree::Admin::WholesalersController < Spree::Admin::ResourceController
     else
       flash[:error] = I18n.t('spree.admin.wholesaler.update_failed')
     end
-    respond_with(@wholesaler)
+    respond_with(@wholesaler) do |format|
+      format.html { redirect_to spree.admin_wholesaler_path(@wholesaler) }
+    end
   end
 
   def destroy
