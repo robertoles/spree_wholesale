8ccbea9d0167f7df6f26e8ee675a19b48026ae40
Jeff Dutil <jdutil21@gmail.com>
2012-08-06 09:58:03 -0400

    Add a hook and missing translations.

--- app/overrides/wholesale-my-orders.rb
+++ app/overrides/wholesale-my-orders.rb
@@ -1,4 +1,3 @@
-#insert_before :account_my_orders,         'hooks/wholesale_customer'
 Deface::Override.new(:virtual_path => 'spree/users/show',
 :name => 'wholesale-my-orders',
 :insert_before => "[data-hook='account_my_orders'], #account_my_orders[data-hook]",
