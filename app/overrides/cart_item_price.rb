f874072976a235b6e92d0551096f1e7ac5ef5ff6
robertoles <robertoles@me.com>
2013-01-28 12:06:04 +0000

    Removing item price override

--- app/overrides/cart_item_price.rb
+++ app/overrides/cart_item_price.rb
@@ -1,8 +1,8 @@
 #replace :cart_item_price,                 'hooks/cart_item_price'
-Deface::Override.new(
-  :virtual_path => 'spree/orders/_line_item',
-  :name => 'cart_item_price',
-  :replace_contents => "[data-hook='cart_item_price'], #cart_item_price[data-hook]",
-  :partial => "spree/hooks/cart_item_price",
-  :disabled => false
-)
+#Deface::Override.new(
+#  :virtual_path => 'spree/orders/_line_item',
+#  :name => 'cart_item_price',
+#  :partial => "spree/hooks/cart_item_price",
+#  :replace_contents => "[data-hook='cart_item_price'], #cart_item_price[data-hook]",
+#  :disabled => false
+#)
